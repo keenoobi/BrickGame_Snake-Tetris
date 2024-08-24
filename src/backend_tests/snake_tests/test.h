7981f32399d66ce965bb405af94ddd8d250c5068
keenoobi <koksharovanwork@gmail.com>
2024-08-24 19:50:33 +0300

    created snake tests and changed makefile so it works well with desktop and cli versions

---
+++ src/backend_tests/snake_tests/test.h
@@ -0,0 +1,8 @@
+#ifndef CPP1_S21_MATRIXPLUS_MATRIX_TESTS_H_
+#define CPP1_S21_MATRIXPLUS_MATRIX_TESTS_H_
+
+#include <gtest/gtest.h>
+
+#include "src/brick_game/snake/backend/snake_model.h"
+
+#endif  // CPP1_S21_MATRIXPLUS_MATRIX_TESTS_H_
